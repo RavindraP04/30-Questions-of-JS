3fc4f129a84f02bd74e13ea1fc4e6c126dd51e56
Ravindra Pandey <ravindrapandey99999@gmail.com>
2023-01-17 23:47:46 +0530

    solved

---
+++ Question 18/app.js
@@ -0,0 +1,8 @@
+//19. Even numbers are divisible by 2 and the remainder is zero. How do you check, if a number is even or not using JavaScript?
+
+let num = prompt("Enter a number:- ");
+if (num%2 == 0) {
+    console.log(num + " is an even number");
+} else {
+    console.log(num + " is an odd number");
+}
