a37f41c9679fa06cd38375b16bbc2b3265df6833
Ravindra Pandey <ravindrapandey99999@gmail.com>
2023-01-17 23:28:04 +0530

    solved

---
+++ Question 15/app.js
@@ -0,0 +1,8 @@
+// 15. Get radius using prompt and calculate the area of a circle (area = pi x r x r) and circumference of a circle(c = 2 x pi x r) where pi = 3.14.
+
+let radius = prompt("Enter the radius");
+let pi = 3.14;
+
+console.log(`Area of circle is:- ${pi*radius*radius}
+    Circumference of circle is:- ${2*pi*radius}
+`);
