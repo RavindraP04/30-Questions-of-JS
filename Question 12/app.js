c5eed7324cb758f71d8801a0056ef3b3ea9ba1c5
Ravindra Pandey <ravindrapandey99999@gmail.com>
2023-01-17 22:34:09 +0530

    solved

---
+++ Question 12/app.js
@@ -0,0 +1,15 @@
+// 12. Use the Date object to do the following activities
+let date = new Date();
+//     - What is the year today?
+console.log("Current year is "+ date.getFullYear());
+//     - What is the month today as a number?
+console.log("Current month is "+(date.getMonth()+1)+"st");
+//     - What is the date today?
+console.log("Current date is "+date.getDate());
+//     - What is the day today as a number?
+console.log("Current day is "+date.getDay());
+//     - What is the hours now?
+console.log("Current hour is "+date.getHours());
+//     - What is the minutes now?
+console.log("Current minute is "+date.getMinutes());
+//     - Find out the numbers of seconds elapsed from January 1, 1970 to now.
