7ad8e477d7f0384c6eb9d62a9d99521131dfcc47
Ravindra Pandey <ravindrapandey99999@gmail.com>
2023-01-17 23:00:35 +0530

    solved

---
+++ Question 14/app.js
@@ -0,0 +1,7 @@
+// 14. Slope is m = (y2-y1)/(x2-x1). Find the slope between point (2, 2) and point(6,10)
+let coordinate1 = [2, 2];
+let coordinate2 = [6, 10];
+
+console.log(`
+    Slope of the line is:- ${(coordinate2[1]-coordinate1[1])/(coordinate2[0]-coordinate1[0])};
+`);
